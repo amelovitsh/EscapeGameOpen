33c7fe727e50bcf76d479be0f60842ce08d59892
pulsar <amelovitsh@gmail.com>
2019-11-05 10:06:16 +0100

    ajout log4j + class log4j+ log4j.properties

---
+++ src/escapeGame/log4j.java
@@ -0,0 +1,15 @@
+package escapeGame;
+
+import org.apache.log4j.Logger;
+public class log4j {
+
+	public static void main(String[] args) {
+		logger.debug("msg de debogage");
+	    logger.info("msg d'information");
+	    logger.warn("msg d'avertissement");
+	    logger.error("msg d'erreur");
+	    logger.fatal("msg d'erreur fatale"); 
+
+	}
+
+}
